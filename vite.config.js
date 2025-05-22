496e3d33bde25ce23fa961e22f367a362188f056
Marco Milanta <marco.milanta@gmail.com>
2025-05-22 18:34:46 +0200

    fix: install tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   // ... other config
   optimizeDeps: {
     exclude: ['algo.js'], // Exclude the WASM module from optimization
   },
+  plugins: [
+    tailwindcss(),
+  ],
 });
