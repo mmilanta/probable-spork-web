2252cdd2873d21222a072c5e07e211a67153063b
mmilanta <marco.milanta@gmail.com>
2025-12-07 14:17:01 +0100

    fix: try something

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/probable-spork-web/', // Set base path for GitHub Pages
   optimizeDeps: {
     exclude: ['algo.js', 'pyodide'], // Exclude both from optimization
   },
